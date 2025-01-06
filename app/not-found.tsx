5dc5edd20b08d2798606b010f523505228fd578e
Etienne <gaumery@hotmail.com>
2025-01-06 14:53:53 +0100

    Initial test1

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Page non trouvée</h1>
+      <p>Désolé, la page que vous recherchez n'existe pas.</p>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur votre projet Next.js avec App Router !</h1>
+      <p>Commencez à personnaliser votre projet ici.</p>
+    </div>
+  );
+}
